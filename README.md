867f56357bf362fe2f22aea00030f639230ff329
bean-frog <104943861+bean-frog@users.noreply.github.com>
2023-05-24 09:04:15 -0700

    make gooder

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ligma.js
-a bunch of random ass thinsg to mess with people
+a bunch of random ass things to mess with people
+### random ass things in question:
+- `ligmajs.inYourWalls(interval, type);` spams "I'm in your walls" every `interval` ms. `type` accepts `message`, `warn`, or `error`
--- ligma.js
+++ ligma.js
@@ -1,51 +1,17 @@
-//uncomment any functions you want to be active, and dont forget to link the script to your html
-
-
-//static errors 1
-/*
-console.error("bro wtf are you doing here there's nothing here like bro just stop its not worth it srsly no no no no no no no no no stop")
-console.error("why are you even here you're not a hacker kid you're 12")
-console.error("FATAL ERROR: DUMBASS IN CONSOLE DETECTED. ENGAGING DRONE STRIKE ON LOCATION")
-*/
-
-//in your walls (infinite)
-/*
-const interval = 60 * 1000; //replace the 60 with the number of seconds to delay
-function conspam() {
-    console.log("") //remove this line to stack messages
-    console.warn("WARNING: I am in your walls")
-}
-setInterval(conspam, interval)
-*/
-
-//angry lol
-/*
-console.log("Hello There, it seems you have entered the JavaScript Console.")
-console.log("Might I ask what you intend to do here?")
-console.warn("I really think you sh0uld leave")
-console.warn("I'm getting angry now")
-console.error("A N G E R Y!!! Kill Mode Activated")
-console.error("Deploying strike aircraft to your calculated geolocation")
-*/
-
-//approaching
-/*
-var i = 999999; //replace with whatever you want
-
-function startTimer() {
-
-    var countdownTimer = setInterval(function() {
-
-        console.warn("I AM " + i + " METERS FROM YOUR LOCATION AND APPROACHING RAPIDLY");
-        i = i - 14; //replace 14 with the amount to decrease each second
-
-        if (i <= 0) {
-            clearInterval(countdownTimer);
-        }
-
-    }, 1000);
-
-}
-
-startTimer();
- */
+var ligmajs = (function() {
+       return {
+      inYourWalls: function(interval, type) {
+        setInterval(function() {
+          if (type === "message") {
+            console.log("I'm in your walls");
+          } else if (type === "warn") {
+            console.warn("I'm in your walls");
+          } else if (type === "error") {
+            console.error("I'm in your walls");
+          } else {
+            console.log("Invalid type specified");
+          }
+        }, interval);
+      }
+    };
+  })();
