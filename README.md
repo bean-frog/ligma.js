ed0e96b91d3f6fe9d227e83408a97b42110a20f7
beanfrog <104943861+bean-frog@users.noreply.github.com>
2023-08-17 10:27:46 -0700

    do stuff i guess

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # ligma.js
 a bunch of random ass things to mess with people
 ### random ass things in question:
-- `ligmajs.inYourWalls(interval, type);` spams "I'm in your walls" every `interval` ms. `type` accepts `message`, `warn`, or `error`
+
+#### general trolling functions:
+- `ligmajs.inYourWalls(interval, type);` spams "I'm in your walls" every `interval` ms. 
+    - `type` accepts `message`, `warn`, or `error`
+- `ligmajs.rajesh(company, amount, cardtype);` sends a popup stating that a tech support agent from `company` needs an `amount` `cardtype` gift card. 
+    - `company` accepts `google`, `microsoft`, `mcafee`, `norton`, or `apple`. `amount` can be any integer. 
+    - `cardtype` accepts `googleplay`, `amazon`, `visa`, or `bassproshop`
+#### malware-ish functions:
+- `ligmajs.lagmachine();` up to 10 seconds of page lagging, randomly every 0-10 seconds.
+
+
--- ligma.js
+++ ligma.js
@@ -9,9 +9,38 @@ var ligmajs = (function() {
           } else if (type === "error") {
             console.error("I'm in your walls");
           } else {
-            console.log("Invalid type specified");
+            console.error("ligmajs error in module 'inYourWalls'. invalid type provided:" + type);
           }
         }, interval);
+      },
+      lagmachine: function() {
+        function lag(n) {
+          var now = new Date().getTime();
+          while( (new Date().getTime()) - now < n*1000 ) {}
       }
+      function run() {
+          lag(Math.floor(Math.random()*10)+1);
+          setTimeout(run, Math.random()*10000);
+      }
+      run();
+      },
+      rajesh: function(company, amount, cardtype) {
+        let validcompanies = ['apple', 'google', 'microsoft', 'norton', 'mcafee'];
+        let validcardtypes = ['googleplay', 'amazon', 'visa', 'bassproshop'];
+        if (validcompanies.includes(company)) {
+          if (validcardtypes.includes(cardtype)) {
+            if(Number.isInteger(amount)) {
+              alert('helo my friend this is adam smith from' + company + 'your computer has a virus and to remove it please send to me a ' + amount + 'dollar' + cardtype + 'card thank you sir')
+            } else {
+              console.error('ligmajs error in module "rajesh". invalid amount arg provided: ' + amount)
+            }
+          } else {
+          console.error('ligmajs error in module "rajesh". invalid cardtype arg provided: ' + cardtype)
+          }
+        } else {
+          console.error('ligmajs error in module "rajesh". invalid company arg provided: ' + company)
+        }
+      },
+
     };
   })();
